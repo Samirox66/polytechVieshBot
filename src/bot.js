1ad799672fe150043ffa30793e30e857fbb5e4bc
Samir <bsam@bk.ru>
2022-04-07 09:51:14 +0300

    deleted webhook for now

--- src/bot.js
+++ src/bot.js
@@ -27,12 +27,7 @@ bot.action('YES', commands.onAddQuestion);
 bot.action('DELETE', commands.onDeleteQuestion);
 
 bot.on('message', commands.message);
-bot.launch({
-  webhook: {
-    domain: 'https://polytech-viesh-bot.herokuapp.com/',
-    port: process.env.PORT || 80,
-  },
-});
+bot.launch();
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
